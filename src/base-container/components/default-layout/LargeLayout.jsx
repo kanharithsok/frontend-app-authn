612f8164d91418843db72b18e2454dff82f73472
ReaksmeyThan <reaksmey.than7up@gmail.com>
2024-07-20 01:11:22 +0700

    update logo

--- src/base-container/components/default-layout/LargeLayout.jsx
+++ src/base-container/components/default-layout/LargeLayout.jsx
@@ -5,6 +5,7 @@ import { useIntl } from '@edx/frontend-platform/i18n';
 import { Hyperlink, Image } from '@openedx/paragon';
 import classNames from 'classnames';
 
+import logo from '../../../assets/logo.png';
 import messages from './messages';
 
 const LargeLayout = () => {
@@ -14,7 +15,7 @@ const LargeLayout = () => {
     <div className="w-50 d-flex">
       <div className="col-md-9 bg-primary-400">
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-          <Image className="logo position-absolute" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
+          <Image className="logo position-absolute" alt={getConfig().SITE_NAME} src={logo} />
         </Hyperlink>
         <div className="min-vh-100 d-flex align-items-center">
           <div className={classNames({ 'large-yellow-line mr-n4.5': getConfig().SITE_NAME === 'edX' })} />
